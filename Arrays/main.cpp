daf60008b351761303d0188a0982e872781337b7
DmitryK777 <kislyakovdmv@gmail.com>
2024-01-28 17:18:37 +0300

    Second commit

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,8 +1,22 @@
-//Arrays
+﻿//Arrays
 #include <iostream>
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+	const int n = 5;
+	int arr[n] = {3, 5, 8};;
+
+	//arr[1] = 1024;
+	//arr[2] = 3072;
+
+	//cout << arr[1] << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
