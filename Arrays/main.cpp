ab16c4d8a000678768b96025fefb79138f1c5e69
DmitryK777 <kislyakovdmv@gmail.com>
2024-01-30 21:53:13 +0300

    Home work

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -7,16 +7,51 @@ void main()
 	setlocale(LC_ALL, "");
 
 	const int n = 5;
-	int arr[n] = {3, 5, 8};;
+	int arr[n];
 
-	//arr[1] = 1024;
-	//arr[2] = 3072;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Введите " << i + 1 << "-й " << "элемент массива: "; cin >> arr[i];
+	}
+	cout << endl;
 
-	//cout << arr[1] << endl;
-
+	cout << "Получившийся массив:" << endl;
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
+	cout << endl << endl;
+
+	cout << "Массив в обратном порядке:" << endl;
+	for (int i = n-1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl << endl;
+
+	cout << "Сумма элементов массива:" << endl;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	cout << sum << endl;
+	cout << endl;
+
+	cout << "Среднее арифметическое элементов массива:" << endl;
+	double average = (double)sum / (double)n;
+	cout << average << endl;
+	cout << endl;
+
+	cout << "Минимальные и максимальные значения в массиве:" << endl;
+	int min = arr[0];
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+		if (arr[i] > max) max = arr[i];
+	}
+	cout << "Минимум:  " << min << endl;
+	cout << "Максимум: " << max << endl;
 	cout << endl;
 }
